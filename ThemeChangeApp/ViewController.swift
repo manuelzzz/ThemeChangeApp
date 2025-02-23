c899098d992a5a0b0855805e5407fb313a31abc1
Manuel Santos Souza <manuel.souza.017@ufrn.edu.br>
2025-02-23 16:16:46 -0300

    Theme change logic

--- ThemeChangeApp/ViewController.swift
+++ ThemeChangeApp/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
+enum Theme {
+	case blue, red, yellow
+}
+
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		changeTheme(to: .blue)
 	}
 
+	func changeTheme(to theme: Theme) {
+		switch theme {
+		case .blue:
+			view.backgroundColor = .blue
+		case .red:
+			view.backgroundColor = .red
+		case .yellow:
+			view.backgroundColor = .yellow
+		}
+	}
+
+	@IBAction func blueThemeButtonTapped(_ sender: Any) {
+		changeTheme(to: .blue)
+	}
+
+	@IBAction func redThemeButtonTapped(_ sender: Any) {
+		changeTheme(to: .red)
+	}
+
+	@IBAction func yellowThemeButtonTapped(_ sender: Any) {
+		changeTheme(to: .yellow)
+	}
 
 }
-
